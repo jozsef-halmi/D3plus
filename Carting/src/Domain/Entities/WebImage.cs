27cee0301a4fd307a683eb2e4b2aa865c2e32394
Jozsef Halmi <halmi.jozsef.90@hotmail.hu>
2022-06-02 14:12:19 +0200

    removing unused namespaces

--- Carting/src/Domain/Entities/WebImage.cs
+++ Carting/src/Domain/Entities/WebImage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Carting.Domain.Entities;
+﻿namespace Carting.Domain.Entities;
 
 public class WebImage
 {
