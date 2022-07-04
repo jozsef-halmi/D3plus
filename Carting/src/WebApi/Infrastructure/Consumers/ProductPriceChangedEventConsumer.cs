387d83764c7407defbcacc1a2af08e5e171f1a18
Jozsef Halmi <halmi.jozsef.90@hotmail.hu>
2022-07-04 21:12:25 +0200

    Adding inital consumer

---
+++ Carting/src/WebApi/Infrastructure/Consumers/ProductPriceChangedEventConsumer.cs
@@ -0,0 +1,34 @@
+﻿namespace Carting.WebApi.Infrastructure.Consumers;
+
+using MassTransit;
+using Messaging.Contracts;
+
+class ProductPriceChangedIntegrationEventConsumer :
+    IConsumer<ProductPriceChangedIntegrationEvent>
+{
+    public async Task Consume(ConsumeContext<ProductPriceChangedIntegrationEvent> context)
+    {
+        var a = 5;
+    }
+}
+
+
+class ProductPriceChangedIntegrationEventConsumerDefinition :
+    ConsumerDefinition<ProductPriceChangedIntegrationEventConsumer>
+{
+    public ProductPriceChangedIntegrationEventConsumerDefinition()
+    {
+        // override the default endpoint name
+        EndpointName = "carting-service";
+    }
+
+    protected override void ConfigureConsumer(IReceiveEndpointConfigurator endpointConfigurator,
+        IConsumerConfigurator<ProductPriceChangedIntegrationEventConsumer> consumerConfigurator)
+    {
+        // configure message retry with millisecond intervals
+        endpointConfigurator.UseMessageRetry(r => r.Intervals(100, 200, 500, 800, 1000));
+
+        // use the outbox to prevent duplicate events from being published
+        endpointConfigurator.UseInMemoryOutbox();
+    }
+}
