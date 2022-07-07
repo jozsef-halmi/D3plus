32df00c2209ff8b429491b3009a800fc870e3bba
Jozsef Halmi <halmi.jozsef.90@hotmail.hu>
2022-07-07 18:13:50 +0200

    Removing in-memory outbox setting

--- Carting/src/WebApi/Infrastructure/Consumers/ProductDeletedEventConsumer.cs
+++ Carting/src/WebApi/Infrastructure/Consumers/ProductDeletedEventConsumer.cs
@@ -57,8 +57,5 @@ class ProductDeletedIntegrationEventConsumerDefinition :
     {
         // configure message retry with millisecond intervals
         endpointConfigurator.UseMessageRetry(r => r.Intervals(100, 200, 500, 800, 1000));
-
-        // use the outbox to prevent duplicate events from being published
-        endpointConfigurator.UseInMemoryOutbox();
     }
 }
--- Carting/src/WebApi/Infrastructure/Consumers/ProductPriceChangedEventConsumer.cs
+++ Carting/src/WebApi/Infrastructure/Consumers/ProductPriceChangedEventConsumer.cs
@@ -60,8 +60,5 @@ class ProductPriceChangedIntegrationEventConsumerDefinition :
     {
         // configure message retry with millisecond intervals
         endpointConfigurator.UseMessageRetry(r => r.Intervals(100, 200, 500, 800, 1000));
-
-        // use the outbox to prevent duplicate events from being published
-        endpointConfigurator.UseInMemoryOutbox();
     }
 }
