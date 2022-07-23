96830560894f5121bec56f29661309def0396017
Jozsef Halmi <halmi.jozsef.90@hotmail.hu>
2022-07-23 11:35:10 +0200

    Adding aggregator for product data

--- ApiGateway/ApiGateway/Program.cs
+++ ApiGateway/ApiGateway/Program.cs
@@ -1,3 +1,4 @@
+using ApiGateway.Aggregators;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -53,7 +54,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot()
+    .AddSingletonDefinedAggregator<GetProductAggregator>();
 builder.Configuration.AddJsonFile("ocelot.json")
     .AddEnvironmentVariables();
 
